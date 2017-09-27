b09cad61f6aa98a68d08a5f29ae16fbf1703b836
Naxiden <bialikandrzej10@gmail.com>
2017-09-27 19:39:46 +0200

    Life-circle exercises

---
+++ proejkt-reactus/life-circle/life-cricluu/src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Component life circle</h1>
+        </header>
+          <Body />
+
+
+      </div>
+    );
+  }
+}
+
+
+class Body extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      r:0
+    };
+    this.getRandomNumber = this.getRandomNumber.bind(this);
+
+  }
+  getRandomNumber (){
+    // console.log("koniek");
+    this.setState({r: Math.floor(Math.random()*10) })
+  }
+
+  render(){
+    return(
+      <div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={this.getRandomNumber}>Random Number:</button>
+        <Numbers myNumber={this.state.r}/>
+      </div>
+    );
+  }
+}
+
+class Numbers extends Component {
+  render(){
+    return(
+      <div>
+        <br />
+        {this.props.myNumber}
+      </div>
+    );
+  }
+}
+
+
+export default App;
